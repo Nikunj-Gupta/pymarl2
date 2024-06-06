19bda485796d2e976efd07f50d404140438901d2
nikunj-gupta <ng2531@nyu.edu>
2024-06-06 16:19:31 -0700

    Patch maco and smacv2 envs

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -1,12 +1,18 @@
 from functools import partial
 import sys
 import os
-
-from .multiagentenv import MultiAgentEnv
+from smacv2.env import StarCraft2Env, StarCraftCapabilityEnvWrapper
+from .multiagentenv import MultiAgentEnv 
 
 from .starcraft import StarCraft2Env
 from .matrix_game import OneStepMatrixGame
-from .stag_hunt import StagHunt
+from .stag_hunt import StagHunt 
+from .maco.aloha import AlohaEnv
+from .maco.pursuit import PursuitEnv
+from .maco.sensors import SensorEnv
+from .maco.hallway import HallwayEnv
+from .maco.disperse import DisperseEnv
+from .maco.gather import GatherEnv
 
 try:
     gfootball = True
@@ -20,6 +26,13 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+REGISTRY["sc2wrapped"] = partial(env_fn, env=StarCraftCapabilityEnvWrapper)
+REGISTRY["aloha"] = partial(env_fn, env=AlohaEnv)
+REGISTRY["pursuit"] = partial(env_fn, env=PursuitEnv)
+REGISTRY["sensor"] = partial(env_fn, env=SensorEnv)
+REGISTRY["hallway"] = partial(env_fn, env=HallwayEnv)
+REGISTRY["disperse"] = partial(env_fn, env=DisperseEnv)
+REGISTRY["gather"] = partial(env_fn, env=GatherEnv)
 REGISTRY["stag_hunt"] = partial(env_fn, env=StagHunt)
 REGISTRY["one_step_matrix_game"] = partial(env_fn, env=OneStepMatrixGame)
 
