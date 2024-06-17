a43fabe363d914c4887a5fb20e9a45974238d8cb
nikunj-gupta <ng2531@nyu.edu>
2024-06-17 14:34:04 -0700

    removing t_max from algo yamls

--- runs_maco.py
+++ runs_maco.py
@@ -1,28 +1,69 @@
 import os 
+PARALLEL = False 
 
+"""
+baselines 
+"""
+def baselines(maps, configs, gpu, seed_max): 
+    for map in maps: 
+        for config in configs: 
+            for _ in range(seed_max): 
+                command = f"CUDA_VISIBLE_DEVICES={gpu} python3 src/main.py --config={config} --env-config={map} " 
+                if PARALLEL: command += " &"
+                print(command) 
+                os.system(command) 
+# baselines(
+#     maps = ["aloha", "disperse", "gather", "hallway", "pursuit", "sensor"], 
+#     configs=["iql", "vdn", "qmix", "cw_qmix", "dop", "ow_qmix", "qatten", "qmix_att", "qplex", "qtran"], 
+#     gpu = 0, 
+#     seed_max=1 
+# )
 
-# configs=["iql", "vdn", "qmix", "coma", "cw_qmix", "dop", "ippo", "lica", "ow_qmix", "qatten", "qmix_att", "qmix_large", "qplex", "qtran", "riit", "riit_online", "vmix"] 
-# maps = ["aloha", "disperse", "gather", "hallway", "pursuit"] 
-# agents = ["gat", "gatv2", "gcn", "gtn"] 
+"""
+cg-marl 
+"""
+def cgmarl(maps, configs, agents, cg_edges, gpu, seed_max): 
+    for map in maps: 
+        for config in configs: 
+            for agent in agents: 
+                for cg_edge in cg_edges: 
+                    for _ in range(seed_max): 
+                        command = f"CUDA_VISIBLE_DEVICES={gpu} python3 src/main.py --config={config} --env-config={map} with agent={agent} cg_edges={cg_edge} "                         
+                        if PARALLEL: command += " &"
+                        print(command) 
+                        # os.system(command) 
+# cgmarl(
+#     maps = ["aloha", "disperse", "gather", "hallway", "pursuit", "sensor"], 
+#     configs=["vdn", "qmix"], 
+#     agents = ["gcn", "gat", "gatv2", "gtn"], 
+#     cg_edges = ["full", "star", "cycle", "line"], 
+#     GPU=0, 
+#     seed_max=1 
+# )
 
-configs=["qmix", "vdn"] 
-maps = ["aloha", "disperse", "gather", "hallway", "pursuit"] 
-agents = ["gtn"] 
-GPU = 0 
-seed_max=1 
+"""
+DMCG 
+"""
+def dmcg(maps, configs, agents, gpu, seed_max): 
+    for map in maps: 
+        for config in configs: 
+            for agent in agents: 
+                for _ in range(seed_max): 
+                    command = f"CUDA_VISIBLE_DEVICES={gpu} python3 src/main.py --config={config} --env-config={map} with agent={agent} "                         
+                    if PARALLEL: command += " &"
+                    print(command) 
+                    os.system(command) 
+# dmcg(
+#     maps = ["sensor"], 
+#     configs=["qmix"], 
+#     agents = ["gtn"], 
+#     gpu=0, 
+#     seed_max=1 
+# )
 
-parallel = True 
-
-# for map in maps: 
-#     for config in configs: 
-#         for agent in agents: 
-#             for _ in range(seed_max): 
-#                 command = f"CUDA_VISIBLE_DEVICES={GPU} python3 src/main.py --config={config} --env-config={map} with agent={agent} cg_edges=full"  
-#                 if parallel: command += " &"
-#                 os.system(command) 
-for map in maps: 
-    for config in configs: 
-        for _ in range(seed_max): 
-            command = f"CUDA_VISIBLE_DEVICES={GPU} python3 src/main.py --config={config} --env-config={map} with cg_edges=full" 
-            if parallel: command += " &"
-            os.system(command) 
+baselines(
+    maps = ["sensor"], 
+    configs=["qmix", "iql", "vdn"], 
+    gpu = 0, 
+    seed_max=1 
+)
